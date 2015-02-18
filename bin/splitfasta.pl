b6b0305ce2a79e538ce86aeacea9ab8125e85373
Malay Basu <malaykbasu@gmail.com>
2015-02-18 13:57:40 -0600

    Modified split fasta.

--- bin/splitfasta.pl
+++ bin/splitfasta.pl
@@ -5,10 +5,13 @@
 # Usage: splitfasta.pl <filename>
 
 use strict;
-
+use Getopt;
 die "Usage: splitfasta.pl <filename>\n" unless $ARGV[0];
+our (%opt);
+#open( INFILE, $ARGV[0] ) || die "Can't open $ARGV[0]\n";
+getopt('s:', \%opt);
 
-open( INFILE, $ARGV[0] ) || die "Can't open $ARGV[0]\n";
+$opt{s} = 1 unless $opt{s};
 
 my $lastseq     = "";
 my $lastgi      = "";
@@ -17,9 +20,14 @@ my $written_seq = 0;
 
 #my $lastdefline;
 
+my $file = shift;
+open (INFILE, $file ) || die "Can't open $file\n";
+
+if ($opt{s} == 1) {
 while ( my $line = <INFILE> ) {
     if ( $line =~ /^>/ ) {
-        $total_seq++;
+	$total_seq++;
+       
         if ($lastgi) {
             my $outfile = $lastgi . '.fas';
             open( OUTFILE, ">$outfile" ) || die "Can't open $outfile\n";
@@ -60,6 +68,12 @@ if ($lastgi) {
     #		$lastgi = $1;
     #		$lastgi =~ s/[^0-9A-Za-z]/\_/g;
 
+}
+}
+
+if ($opt{s} > 1 ) {
+
+    
 }
 
 print STDERR "$total_seq sequences found in the file\n";
